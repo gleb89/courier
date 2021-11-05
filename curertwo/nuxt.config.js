fecf1453e5ad0caa97590395d182a69bebd2547b
gleb89 <glebhleb89@icloud.com>
2021-11-05 21:24:27 +0300

    fgf

--- curertwo/nuxt.config.js
+++ curertwo/nuxt.config.js
@@ -7,9 +7,11 @@ export default {
     title: 'curertwo',
     meta: [
       { charset: 'utf-8' },
+      {name :"apple-mobile-web-app-status-bar-style", content :"default"},
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
+      
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
