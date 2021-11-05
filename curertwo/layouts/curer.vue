1c30cb670131f77a97054c026cefbf9ee9861dd0
gleb89 <glebhleb89@icloud.com>
2021-11-05 15:07:22 +0300

    fgf

--- curertwo/layouts/curer.vue
+++ curertwo/layouts/curer.vue
@@ -21,9 +21,30 @@ export default {
     }
     if (localStorage.getItem('jwtTokenCurer')) {
       this.$store.commit("curer/setAuthadmin", localStorage.getItem('jwtTokenCurer'));
+      this.sendLogin()
         this.$router.push('/curer/ordersall')
       }
     
+    },
+    methods: {
+    sendLogin(){
+        const headers = {
+            "Content-Type": "application/json",
+            Authorization: localStorage.getItem('jwtTokenCurer'),
+          };
+        this.$axios
+        .$get(`couriers/data_courier`, {
+          headers:headers
+        })
+        .then((resp) => {
+          this.$store.commit("curer/setData_zakaz", resp);
+          
+        })
+        .catch(function (error) {
+        console.log('error');
+      });
+        
+     },
     },
 }
 </script>
